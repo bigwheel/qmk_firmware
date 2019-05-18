b8d3b8315a2dedd86f4d20497c140bef1ac69675
bigwheel <k.bigwheel+eng@gmail.com>
2019-05-19 02:56:37 +0900

    リファクタリング

--- keyboards/ergodox_ez/keymaps/109like/keymap.c
+++ keyboards/ergodox_ez/keymaps/109like/keymap.c
@@ -12,43 +12,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.       ,--------------------------------------------------.
- * |   1    |   2  |   3  |   4  |   5  |   6  |      |       | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * |   1    |   2  |   3  |   4  |   5  |   6  | Up   |       | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
  * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |       |  Up  |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |       |      |   Y  |   U  |   I  |   O  |   P  |   @    |
  * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |   ;  |   :    |
  * |--------+------+------+------+------+------| LEFT |       | RIGHT|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |   /  |   \    |
  * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
- *   |  L1  | LGui | LAlt |      | Muhen|                                   |Henkan|   |  |   [  |   ]  |      |
+ *   |  L1  | LGui | LAlt |  App | Muhen|                                   |Henkan|   |  |   [  |   ]  |      |
  *   `----------------------------------'                                   `----------------------------------'
  *                                      ,-------------.       ,-------------.
- *                                      | App  | Home |       | PgUp | Ctrl |
+ *                                      |      | Home |       | PgUp |Insert|
  *                               ,------|------|------|       |------+------+------.
  *                               |      |      | End  |       | PgDn |      |      |
  *                               | Space|Backsp|------|       |------| Del  |Enter |
- *                               |      |ace   | Esc  |       | Alt  |      |      |
+ *                               |      |ace   | Esc  |       |      |      |      |
  *                               `--------------------'       `--------------------'
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_TRNS,
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
-  KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT,
-  MO(SYMB), KC_LGUI, KC_LALT, KC_TRNS, KC_MHEN,
-                                                KC_APP,  KC_HOME,
-                                                         KC_END,
-                                       KC_SPC,  KC_BSPC, KC_ESC,
+  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_UP,
+  KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
+  KC_LCTRL, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
+  KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT,
+  MO(SYMB), KC_LGUI,  KC_LALT, KC_APP,  KC_MHEN,
+                                                 KC_TRNS, KC_HOME,
+                                                          KC_END,
+                                        KC_SPC,  KC_BSPC, KC_ESC,
   // right hand
   KC_PSCR,  KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_UP,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_TRNS,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
             KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_RIGHT, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
                       KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, KC_TRNS,
-  KC_PGUP,  KC_LCTRL,
+  KC_PGUP,  KC_INS,
   KC_PGDN,
-  KC_LALT,  KC_DEL,   KC_ENT
+  KC_TRNS,  KC_DEL,   KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
