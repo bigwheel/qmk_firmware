982dd8395e80bc7ae3b417856a95c4f8e06e004c
bigwheel <k.bigwheel+eng@gmail.com>
2019-05-18 22:34:07 +0900

    コメント・配列のアラインメントを調整

--- keyboards/ergodox_ez/keymaps/109like/keymap.c
+++ keyboards/ergodox_ez/keymaps/109like/keymap.c
@@ -7,88 +7,88 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   1    |   2  |   3  |   4  |   5  |   6  | Hyper|           | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |           |  Up  |   Y  |   U  |   I  |   O  |   P  |   @    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   :    |
- * |--------+------+------+------+------+------| LEFT |           | RIGHT|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   \    |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  | LGui | LAlt |      | Muhen|                                       |Henkan|   |  |   [  |   ]  | ~L2  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,---------------.
- *                                        | App  | Home |       | PgUp |  Ctrl  |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | End  |       | PgDn |        |      |
- *                                 | Space|Backsp|------|       |------|  Del   |Enter |
- *                                 |      |ace   | Esc  |       | Alt  |        |      |
- *                                 `--------------------'       `----------------------'
+ * ,--------------------------------------------------.       ,--------------------------------------------------.
+ * |   1    |   2  |   3  |   4  |   5  |   6  | Hyper|       | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |       |  Up  |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |   ;  |   :    |
+ * |--------+------+------+------+------+------| LEFT |       | RIGHT|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |   /  |   \    |
+ * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
+ *   | ~L1  | LGui | LAlt |      | Muhen|                                   |Henkan|   |  |   [  |   ]  | ~L2  |
+ *   `----------------------------------'                                   `----------------------------------'
+ *                                      ,-------------.       ,-------------.
+ *                                      | App  | Home |       | PgUp | Ctrl |
+ *                               ,------|------|------|       |------+------+------.
+ *                               |      |      | End  |       | PgDn |      |      |
+ *                               | Space|Backsp|------|       |------| Del  |Enter |
+ *                               |      |ace   | Esc  |       | Alt  |      |      |
+ *                               `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
-        // left hand
-        KC_1,           KC_2,         KC_3,   KC_4,   KC_5,   KC_6,   ALL_T(KC_NO),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_DOWN,
-        KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LEFT,
-        KC_FN1,         KC_LGUI,      KC_LALT,KC_TRNS,KC_MHEN,
-                                              ALT_T(KC_APP),  KC_HOME,
-                                                              KC_END,
-                                               KC_SPC,KC_BSPC,KC_ESC,
-        // right hand
-             KC_PSCR,     KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,          KC_EQL,
-             KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
-                                KC_HENK, KC_JYEN,KC_RBRC,KC_BSLS,          KC_FN2,
-             KC_PGUP,        KC_LCTRL,
-             KC_PGDN,
-             KC_LALT,KC_DEL, KC_ENT
-    ),
+  // left hand
+  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,          ALL_T(KC_NO),
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_DOWN,
+  KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_LEFT,
+  KC_FN1,   KC_LGUI, KC_LALT, KC_TRNS, KC_MHEN,
+                                                ALT_T(KC_APP), KC_HOME,
+                                                               KC_END,
+                                       KC_SPC,  KC_BSPC,       KC_ESC,
+  // right hand
+  KC_PSCR,  KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_UP,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+            KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_RIGHT, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
+                    KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, KC_FN2,
+  KC_PGUP,  KC_LCTRL,
+  KC_PGDN,
+  KC_LALT,  KC_DEL, KC_ENT
+),
 /* Keymap 1: Symbol Layer
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  | Ver. |           |      |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Reset |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
+ * ,--------------------------------------------------.       ,--------------------------------------------------.
+ * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  | Ver. |       |      |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
+ * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
+ * |        |   !  |   @  |   {  |   }  |   |  |      |       |      |   Up |   7  |   8  |   9  |   *  |        |
+ * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
+ * |  Reset |   #  |   $  |   (  |   )  |   `  |------|       |------| Down |   4  |   5  |   6  |   +  |        |
+ * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
+ * |        |   %  |   ^  |   [  |   ]  |   ~  |      |       |      |   &  |   1  |   2  |   3  |   \  |        |
+ * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                   |      |    . |   0  |   =  |      |
+ *   `----------------------------------'                                   `----------------------------------'
+ *                                      ,-------------.       ,-------------.
+ *                                      |      |      |       |      |      |
+ *                               ,------|------|------|       |------+------+------.
+ *                               |      |      |      |       |      |      |      |
+ *                               |      |      |------|       |------|      |      |
+ *                               |      |      |      |       |      |      |      |
+ *                               `--------------------'       `--------------------'
  */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
-       // left hand
-       KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  M(0),
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       RESET,  KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
-       // right hand
-       KC_TRNS, KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_TRNS,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+  // left hand
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   M(0),
+  KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
+  RESET,   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
+  KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                               KC_TRNS, KC_TRNS,
+                                                        KC_TRNS,
+                                      KC_TRNS, KC_TRNS, KC_TRNS,
+  // right hand
+  KC_TRNS, KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11,  KC_F12,
+  KC_TRNS, KC_UP,   KC_7,    KC_8,   KC_9,   KC_ASTR, KC_TRNS,
+           KC_DOWN, KC_4,    KC_5,   KC_6,   KC_PLUS, KC_TRNS,
+  KC_TRNS, KC_AMPR, KC_1,    KC_2,   KC_3,   KC_BSLS, KC_TRNS,
+                    KC_TRNS, KC_DOT, KC_0,   KC_EQL,  KC_TRNS,
+  KC_TRNS, KC_TRNS,
+  KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS
 ),
 };
 
