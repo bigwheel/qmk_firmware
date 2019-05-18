009638046c64ee47ef2e04241d65dfebf7e23c70
bigwheel <k.bigwheel+eng@gmail.com>
2019-05-19 01:45:45 +0900

    リファクタリング2

--- keyboards/ergodox_ez/keymaps/109like/keymap.c
+++ keyboards/ergodox_ez/keymaps/109like/keymap.c
@@ -4,6 +4,10 @@
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 
+enum custom_keycodes {
+  VRSN
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -28,23 +32,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,          KC_TRNS,
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_DOWN,
+  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_TRNS,
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
   KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_LEFT,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT,
   KC_FN1,   KC_LGUI, KC_LALT, KC_TRNS, KC_MHEN,
-                                                ALT_T(KC_APP), KC_HOME,
-                                                               KC_END,
-                                       KC_SPC,  KC_BSPC,       KC_ESC,
+                                                KC_APP,  KC_HOME,
+                                                         KC_END,
+                                       KC_SPC,  KC_BSPC, KC_ESC,
   // right hand
-  KC_PSCR,  KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_UP,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-            KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_RIGHT, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
-                    KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, KC_TRANS,
+  KC_PSCR,  KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_UP,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+            KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_RIGHT, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
+                      KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, KC_TRNS,
   KC_PGUP,  KC_LCTRL,
   KC_PGDN,
-  KC_LALT,  KC_DEL, KC_ENT
+  KC_LALT,  KC_DEL,   KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -69,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox(
   // left hand
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   M(0),
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   VRSN,
   KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
   RESET,   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
   KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
@@ -93,18 +97,16 @@ const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),               // FN1 - Momentary Layer 1 (Symbols)
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
-      }
-    return MACRO_NONE;
-};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case VRSN:
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        return false;
+    }
+  }
+  return true;
+}
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
