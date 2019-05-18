51166c2d1370f3ce3c31d566210f24e9daf3fc27
bigwheel <k.bigwheel+eng@gmail.com>
2019-05-19 02:12:54 +0900

    リファクタリング

--- keyboards/ergodox_ez/keymaps/109like/keymap.c
+++ keyboards/ergodox_ez/keymaps/109like/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| LEFT |       | RIGHT|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |   /  |   \    |
  * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
- *   | ~L1  | LGui | LAlt |      | Muhen|                                   |Henkan|   |  |   [  |   ]  |      |
+ *   |  L1  | LGui | LAlt |      | Muhen|                                   |Henkan|   |  |   [  |   ]  |      |
  *   `----------------------------------'                                   `----------------------------------'
  *                                      ,-------------.       ,-------------.
  *                                      | App  | Home |       | PgUp | Ctrl |
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
   KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT,
-  KC_FN1,   KC_LGUI, KC_LALT, KC_TRNS, KC_MHEN,
+  MO(SYMB), KC_LGUI, KC_LALT, KC_TRNS, KC_MHEN,
                                                 KC_APP,  KC_HOME,
                                                          KC_END,
                                        KC_SPC,  KC_BSPC, KC_ESC,
@@ -55,13 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.       ,--------------------------------------------------.
  * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  | Ver. |       |      |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
  * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |       |      |   Up |   7  |   8  |   9  |   *  |        |
+ * |        |      |      |      |      |      |      |       |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
- * |  Reset |   #  |   $  |   (  |   )  |   `  |------|       |------| Down |   4  |   5  |   6  |   +  |        |
+ * |  Reset |      |      |      |      |      |------|       |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |       |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |        |      |      |      |      |      |      |       |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                   |      |    . |   0  |   =  |      |
+ *   |      |      |      |      |      |                                   |      |      |      |      |      |
  *   `----------------------------------'                                   `----------------------------------'
  *                                      ,-------------.       ,-------------.
  *                                      |      |      |       |      |      |
@@ -74,27 +74,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox(
   // left hand
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   VRSN,
-  KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
-  RESET,   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
-  KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                KC_TRNS, KC_TRNS,
                                                         KC_TRNS,
                                       KC_TRNS, KC_TRNS, KC_TRNS,
   // right hand
-  KC_TRNS, KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11,  KC_F12,
-  KC_TRNS, KC_UP,   KC_7,    KC_8,   KC_9,   KC_ASTR, KC_TRNS,
-           KC_DOWN, KC_4,    KC_5,   KC_6,   KC_PLUS, KC_TRNS,
-  KC_TRNS, KC_AMPR, KC_1,    KC_2,   KC_3,   KC_BSLS, KC_TRNS,
-                    KC_TRNS, KC_DOT, KC_0,   KC_EQL,  KC_TRNS,
+  KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS
 ),
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),               // FN1 - Momentary Layer 1 (Symbols)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
