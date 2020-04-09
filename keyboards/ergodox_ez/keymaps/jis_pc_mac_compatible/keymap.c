c9aec49eaaead3009ff7853017591cbb62f72885
k.bigwheel <k.bigwheel+eng@gmail.com>
2020-04-09 23:50:38 +0900

    おおよそ動作することを確認

--- keyboards/ergodox_ez/keymaps/jis_pc_mac_compatible/keymap.c
+++ keyboards/ergodox_ez/keymaps/jis_pc_mac_compatible/keymap.c
@@ -1,14 +1,14 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
-#define PC   0 // default layer
+#define PC   0 // pc layer
 #define SYMB 1 // symbols
-
-enum custom_keycodes {
-  VRSN
-};
+#define MAC  2 // mac layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+
+
 /* Keymap 0: PC layer
  *
  * ,--------------------------------------------------.       ,--------------------------------------------------.
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| LEFT |       | RIGHT|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |   /  |   \    |
  * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
- *   |  L1  | LGui | LAlt |  App | Muhen|                                   |Henkan|   |  |   [  |   ]  |      |
+ *   |  L1  | LGui | LAlt |  App | Muhen|                                   |Henkan|   |  |   [  |   ]  |DF MAC|
  *   `----------------------------------'                                   `----------------------------------'
  *                                      ,-------------.       ,-------------.
  *                                      |      | Home |       | PgUp |Insert|
@@ -45,15 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
             KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_RIGHT, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
-                      KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, KC_TRNS,
+                      KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, DF(MAC),
   KC_PGUP,  KC_INS,
   KC_PGDN,
   KC_TRNS,  KC_DEL,   KC_ENT
 ),
+
+
+
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.       ,--------------------------------------------------.
- * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  | Ver. |       |      |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
+ * |   F1   |  F2  |  F3  |  F4  |  F5  |  F6  |      |       |      |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
  * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |       |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
@@ -73,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox(
   // left hand
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   VRSN,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -91,6 +94,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS
 ),
+
+
+
+/* Keymap 2: Mac layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   1    |   2  |   3  |   4  |   5  |   6  |  Up  |           | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |           |      |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   :    |
+ * |--------+------+------+------+------+------| LEFT |           | RIGHT|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   \    |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |  L1  | LAlt | LGui |      | Eisu |                                       | Kana |   |  |   [  |   ]  |DF PC |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        | App  | Home |       | PgUp |  Ctrl  |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 | Space|Backsp|------|       |------|  Del   |Enter |
+ *                                 |      |ace   | Esc  |       | Alt  |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[MAC] = LAYOUT_ergodox(  // layer 2 : mac
+  // left hand
+  KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,    KC_UP,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_DOWN,
+  KC_LCTRL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,    KC_LEFT,
+  MO(SYMB), KC_LALT,  KC_LGUI,  KC_TRNS, KC_LANG2,
+                                                  KC_APP,  KC_HOME,
+                                                           KC_END,
+                                         KC_SPC,  KC_BSPC, KC_ESC,
+  // right hand
+  KC_PSCR,  KC_7,     KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TRNS,  KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,
+            KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_RIGHT, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
+                      KC_LANG1, KC_JYEN, KC_RBRC, KC_BSLS, DF(PC),
+  KC_PGUP,  KC_LCTRL,
+  KC_PGDN,
+  KC_LALT,  KC_DEL,   KC_ENT
+),
+
+
+
 };
 
 // Runs whenever there is a layer state change.
