d83b27d1cd23b864f57d0b748e89c8012d68d283
k.bigwheel <k.bigwheel+eng@gmail.com>
2020-04-10 00:07:39 +0900

    完成

--- keyboards/ergodox_ez/keymaps/jis_pc_mac_compatible/keymap.c
+++ keyboards/ergodox_ez/keymaps/jis_pc_mac_compatible/keymap.c
@@ -11,44 +11,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 0: PC layer
  *
- * ,--------------------------------------------------.       ,--------------------------------------------------.
- * |   1    |   2  |   3  |   4  |   5  |   6  | Up   |       | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
- * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |       |      |   Y  |   U  |   I  |   O  |   P  |   @    |
- * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |   ;  |   :    |
- * |--------+------+------+------+------+------| LEFT |       | RIGHT|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |   /  |   \    |
- * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
- *   |  L1  | LGui | LAlt |  App | Muhen|                                   |Henkan|   |  |   [  |   ]  |DF MAC|
- *   `----------------------------------'                                   `----------------------------------'
- *                                      ,-------------.       ,-------------.
- *                                      |      | Home |       | PgUp |Insert|
- *                               ,------|------|------|       |------+------+------.
- *                               |      |      | End  |       | PgDn |      |      |
- *                               | Space|Backsp|------|       |------| Del  |Enter |
- *                               |      |ace   | Esc  |       |      |      |      |
- *                               `--------------------'       `--------------------'
+ * ,--------------------------------------------------. ,--------------------------------------------------.
+ * |   1    |   2  |   3  |   4  |   5  |   6  | Up   | | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * |--------+------+------+------+------+-------------| |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down | |      |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * |--------+------+------+------+------+------|      | |      |------+------+------+------+------+--------|
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------| |------|   H  |   J  |   K  |   L  |   ;  |   :    |
+ * |--------+------+------+------+------+------| LEFT | | RIGHT|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      | |      |   N  |   M  |   ,  |   .  |   /  |   \    |
+ * `--------+------+------+------+------+-------------' `-------------+------+------+------+------+--------'
+ *   |  L1  | LGui | LAlt |  App | Muhen|                             |Henkan|   |  |   [  |   ]  |DF MAC|
+ *   `----------------------------------'                             `----------------------------------'
+ *                                      ,-------------. ,-------------.
+ *                                      |      | Home | | PgUp |Insert|
+ *                               ,------|------|------| |------+------+------.
+ *                               |      |      | End  | | PgDn |      |      |
+ *                               | Space|Backsp|------| |------| Del  |Enter |
+ *                               |      |ace   | Esc  | |      |      |      |
+ *                               `--------------------' `--------------------'
  */
 [PC] = LAYOUT_ergodox(
   // left hand
-  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_UP,
-  KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
-  KC_LCTRL, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
-  KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT,
-  MO(SYMB), KC_LGUI,  KC_LALT, KC_APP,  KC_MHEN,
-                                                 KC_TRNS, KC_HOME,
-                                                          KC_END,
-                                        KC_SPC,  KC_BSPC, KC_ESC,
+  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_UP,
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,
+  KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT,
+  MO(SYMB), KC_LGUI, KC_LALT, KC_APP,  KC_MHEN,
+                                                KC_TRNS, KC_HOME,
+                                                         KC_END,
+                                       KC_SPC,  KC_BSPC, KC_ESC,
   // right hand
-  KC_PSCR,  KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_TRNS,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-            KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_RIGHT, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
-                      KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, DF(MAC),
+  KC_PSCR,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TRNS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_RIGHT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
+                     KC_HENK, KC_JYEN, KC_RBRC, KC_BSLS, DF(MAC),
   KC_PGUP,  KC_INS,
   KC_PGDN,
-  KC_TRNS,  KC_DEL,   KC_ENT
+  KC_TRNS,  KC_DEL,  KC_ENT
 ),
 
 
@@ -99,46 +99,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 2: Mac layer
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   1    |   2  |   3  |   4  |   5  |   6  |  Up  |           | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |           |      |   Y  |   U  |   I  |   O  |   P  |   @    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   :    |
- * |--------+------+------+------+------+------| LEFT |           | RIGHT|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   \    |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  | LAlt | LGui |      | Eisu |                                       | Kana |   |  |   [  |   ]  |DF PC |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,---------------.
- *                                        | App  | Home |       | PgUp |  Ctrl  |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | End  |       | PgDn |        |      |
- *                                 | Space|Backsp|------|       |------|  Del   |Enter |
- *                                 |      |ace   | Esc  |       | Alt  |        |      |
- *                                 `--------------------'       `----------------------'
+ * ,--------------------------------------------------. ,--------------------------------------------------.
+ * |   1    |   2  |   3  |   4  |   5  |   6  |  Up  | | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * |--------+------+------+------+------+-------------| |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down | |      |   Y  |   U  |   I  |   O  |   P  |   @    |
+ * |--------+------+------+------+------+------|      | |      |------+------+------+------+------+--------|
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------| |------|   H  |   J  |   K  |   L  |   ;  |   :    |
+ * |--------+------+------+------+------+------| LEFT | | RIGHT|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      | |      |   N  |   M  |   ,  |   .  |   /  |   \    |
+ * `--------+------+------+------+------+-------------' `-------------+------+------+------+------+--------'
+ *   |  L1  | LAlt | LGui |      | Eisu |                             | Kana |   |  |   [  |   ]  |DF PC |
+ *   `----------------------------------'                             `----------------------------------'
+ *                                      ,-------------. ,-------------.
+ *                                      | App  | Home | | PgUp |Insert|
+ *                               ,------|------|------| |------+------+------.
+ *                               |      |      | End  | | PgDn |      |      |
+ *                               | Space|Backsp|------| |------| Del  |Enter |
+ *                               |      |ace   | Esc  | |      |      |      |
+ *                               `--------------------' `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [MAC] = LAYOUT_ergodox(  // layer 2 : mac
   // left hand
-  KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,    KC_UP,
-  KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_DOWN,
-  KC_LCTRL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,    KC_LEFT,
-  MO(SYMB), KC_LALT,  KC_LGUI,  KC_TRNS, KC_LANG2,
-                                                  KC_APP,  KC_HOME,
-                                                           KC_END,
-                                         KC_SPC,  KC_BSPC, KC_ESC,
+  KC_1,     KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_UP,
+  KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,    KC_DOWN,
+  KC_LCTRL, KC_A,    KC_S,     KC_D,    KC_F,    KC_G,
+  KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_LEFT,
+  MO(SYMB), KC_LALT, KC_LGUI,  KC_TRNS, KC_LANG2,
+                                                 KC_APP,  KC_HOME,
+                                                          KC_END,
+                                        KC_SPC,  KC_BSPC, KC_ESC,
   // right hand
-  KC_PSCR,  KC_7,     KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_TRNS,  KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,
-            KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_RIGHT, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
-                      KC_LANG1, KC_JYEN, KC_RBRC, KC_BSLS, DF(PC),
-  KC_PGUP,  KC_LCTRL,
+  KC_PSCR,  KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TRNS,  KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,
+            KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_RIGHT, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RO,
+                     KC_LANG1, KC_JYEN, KC_RBRC, KC_BSLS, DF(PC),
+  KC_PGUP,  KC_INS,
   KC_PGDN,
-  KC_LALT,  KC_DEL,   KC_ENT
+  KC_TRNS,  KC_DEL,  KC_ENT
 ),
 
 
