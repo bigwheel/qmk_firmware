723a53d139bd684b07c754f6be3a39841b05ea6d
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-05-28 01:02:36 +0900

    データを修正

--- keyboards/halberd/keymaps/con4t4ble/jtu_custom_keycodes.c
+++ keyboards/halberd/keymaps/con4t4ble/jtu_custom_keycodes.c
@@ -14,19 +14,19 @@ typedef struct {
 
 keycode_mapping kms[] = {
     { JU_2, { KC_2, false }, { KC_LBRC, false } },
-    { JU_6, { KC_EQL, false }, { KC_6, false } },
-    { JU_7, { KC_6, true }, { KC_7, false } },
-    { JU_8, { KC_QUOT, true }, { KC_8, false } },
-    { JU_9, { KC_8, true }, { KC_9, false } },
-    { JU_0, { KC_9, true }, { KC_0, false } },
-    { JU_MINS, { KC_INT1, true }, { KC_MINS, false } },
-    { JU_EQL, { KC_SCLN, true }, { KC_MINS, true } },
-    { JU_LBRC, { KC_RBRC, true }, { KC_RBRC, false } },
-    { JU_RBRC, { KC_NUHS, true }, { KC_NUHS, false } },
-    { JU_BSLS, { KC_INT3, true }, { KC_INT1, false } },
-    { JU_SCLN, { KC_QUOT, false }, { KC_SCLN, false } },
-    { JU_QUOT, { KC_2, true }, { KC_7, true } },
-    { JU_GRV, { KC_EQL, true }, { KC_LBRC, true } },
+    { JU_6, { KC_6, false }, { KC_EQL, false } },
+    { JU_7, { KC_7, false }, { KC_6, true } },
+    { JU_8, { KC_8, false }, { KC_QUOT, true } },
+    { JU_9, { KC_9, false }, { KC_8, true } },
+    { JU_0, { KC_0, false }, { KC_9, true } },
+    { JU_MINS, { KC_MINS, false }, { KC_INT1, true } },
+    { JU_EQL, { KC_MINS, true }, { KC_SCLN, true } },
+    { JU_LBRC, { KC_RBRC, false }, { KC_RBRC, true } },
+    { JU_RBRC, { KC_NUHS, false }, { KC_NUHS, true } },
+    { JU_BSLS, { KC_INT1, false }, { KC_INT3, true } },
+    { JU_SCLN, { KC_SCLN, false }, { KC_QUOT, false } },
+    { JU_QUOT, { KC_7, true }, { KC_2, true } },
+    { JU_GRV, { KC_LBRC, true }, { KC_EQL, true } },
 };
 
 
