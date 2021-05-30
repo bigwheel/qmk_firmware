fb614e5a3400e50ed8ea57870dd3e1ae20a32acf
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-05-30 17:32:30 +0900

    ファイル名を変更

--- keyboards/halberd/keymaps/con4t4ble/keymap.c
+++ keyboards/halberd/keymaps/con4t4ble/keymap.c
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-#include "jtu_custom_keycodes.h"
+#include "mimic_keymap.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LGUI_T(KC_SLSH), KC_BSPC, KC_MHEN, KC_SPC, MO(2), MO(3), KC_ENT, KC_HENK),
