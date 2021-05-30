15b7931fc248a6c4b2eba8020d25c66d2fb5c257
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-05-30 23:52:01 +0900

    バグを一つ取った

--- keyboards/halberd/keymaps/con4t4ble/features.c
+++ keyboards/halberd/keymaps/con4t4ble/features.c
@@ -5,8 +5,20 @@
 #define PROCESS_USUAL_BEHAVIOR      (true)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+#endif
+
     if (process_record_user_mimic(keycode, record) == PROCESS_OVERRIDE_BEHAVIOR)
         return PROCESS_OVERRIDE_BEHAVIOR;
     else
         return PROCESS_USUAL_BEHAVIOR;
 }
+
+// void keyboard_post_init_user(void) {
+//   // Customise these values to desired behaviour
+//   debug_enable=true;
+//   //debug_matrix=true;
+//   debug_keyboard=true;
+//   //debug_mouse=true;
+// }
