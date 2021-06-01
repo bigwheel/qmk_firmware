7999c9c93d2d9e45812a4284d81f230af8239ae2
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-01 23:57:05 +0900

    リファクタリング

--- keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.c
+++ keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.c
@@ -11,8 +11,7 @@ enum custom_keycodes {
 
 bool dispel_is_pressing = false;
 
-// TODO あとで以下のところの処理をこの関数へ置き換える
-bool exist_in_array(uint16_t elem, uint16_t* elems, int size) {
+bool exist_in_array(uint16_t elem, const uint16_t* elems, int size) {
     for (int i = 0; i < size; i++)
         if (elems[i] == elem)
             return true;
@@ -25,28 +24,17 @@ bool process_record_user_auto_disable_ime(uint16_t keycode, keyrecord_t *record)
         return PROCESS_OVERRIDE_BEHAVIOR;
     }
 
-    if (!dispel_is_pressing) {
-        bool disabling_ime = false;
-        for (int i = 0; i < length_of_disabling_ime_keys; i++)
-            if (disabling_ime_keys[i] == keycode) {
-                disabling_ime = true;
-                break;
-            }
-        // https://www.reddit.com/r/olkb/comments/covpq3/problem_checking_for_modifier_key_on_custom_key/
-        if (get_mods() & MOD_MASK_SHIFT)
-            for (int i = 0; i < length_of_disabling_ime_keys_with_shift; i++)
-                if (disabling_ime_keys_with_shift[i] == keycode) {
-                    disabling_ime = true;
-                    break;
-                }
-
-        if (disabling_ime && record->event.pressed) {
+    if (record->event.pressed && !dispel_is_pressing)
+        if (
+                exist_in_array(keycode, disabling_ime_keys, length_of_disabling_ime_keys)
+                ||
+                ((get_mods() & MOD_MASK_SHIFT) && exist_in_array(keycode, disabling_ime_keys_with_shift, length_of_disabling_ime_keys_with_shift))
+           ) {
             uint8_t real_mods_memory = get_mods();
             clear_mods();
             disable_ime();
             set_mods(real_mods_memory);
         }
-    }
 
     return PROCESS_USUAL_BEHAVIOR;
 }
