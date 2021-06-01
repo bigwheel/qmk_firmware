c82120d03d12a9d1e89790056890b43a3a797dbb
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-01 23:42:50 +0900

    自動IME無効化を再び使い始めた

--- keyboards/halberd/keymaps/con4t4ble/features.c
+++ keyboards/halberd/keymaps/con4t4ble/features.c
@@ -10,12 +10,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
 
     process_record_user_auto_disable_ime_timer();
-    /*
+
     if (process_record_user_auto_disable_ime(keycode, record) == PROCESS_OVERRIDE_BEHAVIOR)
         return PROCESS_OVERRIDE_BEHAVIOR;
-    else
-        return PROCESS_USUAL_BEHAVIOR;
-        */
 
     if (process_record_user_mimic(keycode, record) == PROCESS_OVERRIDE_BEHAVIOR)
         return PROCESS_OVERRIDE_BEHAVIOR;
@@ -28,9 +25,9 @@ void matrix_scan_user(void) {
 }
 
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  //debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
+    // Customise these values to desired behaviour
+    debug_enable=true;
+    //debug_matrix=true;
+    debug_keyboard=true;
+    //debug_mouse=true;
 }
