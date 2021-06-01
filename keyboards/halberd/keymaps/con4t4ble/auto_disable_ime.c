26adbad4c13393745331cb4a55e7b6b45f97ee6a
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-02 06:08:13 +0900

    配列の長さはマクロで計算することにした

--- keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.c
+++ keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.c
@@ -31,7 +31,7 @@ bool process_record_user_auto_disable_ime(uint16_t keycode, keyrecord_t *record)
         if (
                 (get_mods() & MOD_MASK_CSAG)
                 ||
-                exist_in_array(keycode, disabling_ime_keys, length_of_disabling_ime_keys)
+                exist_in_array(keycode, disabling_ime_keys, COUNT_OF(disabling_ime_keys))
            ) {
             uint8_t real_mods_memory = get_mods();
             clear_mods();
