ef1dac2d6433acc4842e440d61d24e7e0bd68a27
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-01 22:41:07 +0900

    自動IME無効化機能を独立したコードファイルへ抽出

--- keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.c
+++ keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.c
@@ -127,23 +127,7 @@ const int length_of_leave_ime_on_keys_with_shift =
 
 bool dispel_is_pressing = false;
 
-// 参考元: https://beta.docs.qmk.fm/using-qmk/advanced-keycodes/feature_macros#super-alt-tab
-bool ime_is_disabled_automatically = false;
-uint16_t last_key_record_time = 0;
-#define IME_DISABLED_TIME 10000
-
-void matrix_scan_user(void) {
-  if (ime_is_disabled_automatically == false)
-    if (timer_elapsed(last_key_record_time) > IME_DISABLED_TIME) {
-      disable_ime();
-      ime_is_disabled_automatically = true;
-    }
-}
-
 bool process_record_user_auto_disable_ime(uint16_t keycode, keyrecord_t *record) {
-  last_key_record_time = timer_read();
-  ime_is_disabled_automatically = false;
-
   if (keycode == KC_DISPEL) {
     dispel_is_pressing = record->event.pressed;
     return PROCESS_USUAL_BEHAVIOR;
