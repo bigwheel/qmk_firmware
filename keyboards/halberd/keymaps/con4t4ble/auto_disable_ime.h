5e725e636f04bd9fb8af8625e2a9e61ca85e349c
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-05 20:45:45 +0900

    KC_DISPELを追加

--- keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.h
+++ keyboards/halberd/keymaps/con4t4ble/auto_disable_ime.h
@@ -2,4 +2,8 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    KC_DISPEL = SAFE_RANGE, // 記号を押したときのIME無効化の挙動を打ち消す
+};
+
 bool process_record_user_auto_disable_ime(uint16_t keycode, keyrecord_t *record);
