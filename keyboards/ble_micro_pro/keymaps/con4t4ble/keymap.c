ee1dbefc732800adea8c46d9c20b53e4749f3b11
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-06 01:17:28 +0900

    con4t4bleキーマップでuserspaceへ移動したものを削除

--- keyboards/ble_micro_pro/keymaps/con4t4ble/keymap.c
+++ keyboards/ble_micro_pro/keymaps/con4t4ble/keymap.c
@@ -1,37 +1,4 @@
-/* Copyright 2021 k.bigwheel
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
-#include "bmp.h"
-#include "bmp_custom_keycode.h"
-#include "keycode_str_converter.h"
-#include <stdbool.h>
-
-#include "./auto_disable_ime.h"
-#include "./jtu_custom_keycodes.h"
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    CUSTOM_KEYCODE_START = JTU_SAFE_RANGE,
-};
-
-const key_string_map_t custom_keys_user = {
-    .start_kc    = JTU_START_KC,
-    .end_kc      = JTU_END_KC,
-    .key_strings = JTU_KEY_STRINGS
-};
 
 // デフォルトのキーマップでこれを使用することはほぼないが、一応残しておく
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,29 +8,4 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint32_t keymaps_len() {
   return sizeof(keymaps)/sizeof(uint16_t);
-}
-
-#define PROCESS_OVERRIDE_BEHAVIOR (false)
-#define PROCESS_USUAL_BEHAVIOR (true)
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    process_record_user_auto_disable_ime();
-
-    if (process_record_user_bmp(keycode, record) == PROCESS_OVERRIDE_BEHAVIOR)
-        return PROCESS_OVERRIDE_BEHAVIOR;
-
-    if (process_record_user_jtu(keycode, record) == PROCESS_OVERRIDE_BEHAVIOR)
-        return PROCESS_OVERRIDE_BEHAVIOR;
-
-    return PROCESS_USUAL_BEHAVIOR;
-}
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-    matrix_scan_user_auto_disable_ime();
-}
-
-void led_set_user(uint8_t usb_led) {
 }
--- keyboards/ble_micro_pro/keymaps/con4t4ble/rules.mk
+++
@@ -1,2 +0,0 @@
-# Add jtu_custom_keycodes.c
-SRC += jtu_custom_keycodes.c
