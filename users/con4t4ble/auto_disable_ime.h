f5e9cac2e913387729797e832bfba60b49a46741
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-07-19 01:11:56 +0900

    mimic keymapを有効無効制御できるキーを追加

--- users/con4t4ble/auto_disable_ime.h
+++ users/con4t4ble/auto_disable_ime.h
@@ -2,8 +2,10 @@
 
 #include QMK_KEYBOARD_H
 
+// TODO: 以下のところ、本来ここにあるべきではないmimic keyboardの情報が入っていて良くない
 enum custom_keycodes {
-    KC_DISPEL = SAFE_RANGE, // 記号を押したときのIME無効化の挙動を打ち消す
+    KC_DISPEL = SAFE_RANGE,  // 記号を押したときのIME無効化の挙動を打ち消す
+    KC_TGL_MIMIC,            // mimic keymap機能をON / OFFする
 };
 
 bool process_record_user_auto_disable_ime(uint16_t keycode, keyrecord_t *record);
