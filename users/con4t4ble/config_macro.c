35a946cb516122dd5b166068f3fb3f16de3ff406
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-08-22 01:38:16 +0900

    mimic機能を一時的に無効化するためのキーを追加

--- users/con4t4ble/config_macro.c
+++ users/con4t4ble/config_macro.c
@@ -9,5 +9,7 @@ combo_t key_combos[COMBO_COUNT] = {
     // マクロのせいか複合リテラルが使えない
     // 以下はコンパイルが通るものの解釈がうまく行っていないのか
     // 結局動かない
+    // 追記: process_user_key などで挙動をいじっているとcombo keyが
+    // 正常に動かないと過去の調査でわかった、はず
     // COMBO(((uint16_t [3]){ KC_ESC, KC_Q, COMBO_END }), KC_TAB)
 };
