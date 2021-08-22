e3bb2f33686bbdba6023bca9a3e523200f4a2aa8
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-08-22 11:03:29 +0900

    ./はそこそこ入力するのでダブルではなくトリプルキーに変更

--- users/con4t4ble/config_macro.c
+++ users/con4t4ble/config_macro.c
@@ -1,13 +1,11 @@
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM combo_inputs[COMBO_COUNT][3] = {
-    { KC_TAB, KC_Q, COMBO_END },
-    { KC_DOT, KC_SLSH, COMBO_END },
-    { KC_L, KC_SCLN, COMBO_END },
-};
+const uint16_t PROGMEM combo_inputs0[] = { KC_TAB, KC_Q, COMBO_END };
+const uint16_t PROGMEM combo_inputs1[] = { KC_COMMA, KC_DOT, KC_SLSH, COMBO_END };
+const uint16_t PROGMEM combo_inputs2[] = { KC_L, KC_SCLN, COMBO_END };
 
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(combo_inputs[0], KC_ESC),
-    COMBO(combo_inputs[1], KC_LGUI),
-    COMBO(combo_inputs[2], KC_LALT),
+    COMBO(combo_inputs0, KC_ESC),
+    COMBO(combo_inputs1, KC_LGUI),
+    COMBO(combo_inputs2, KC_LALT),
 };
