ae7690cbfcaae7cdaa2c15cd7f5351744caf4649
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-17 22:16:20 +0900

    コメントをusers下で汎用化したものに合わせて修正

--- users/con4t4blev2/config.h
+++ users/con4t4blev2/config.h
@@ -1,9 +1,9 @@
 #pragma once
 
-// halberdはなぜかキーボード組み込み設定でTAPPING_TERM 100という
-// 超絶シビア設定になっている(´・ω・｀)ので一度設定を取り除く
+// halberdでTAPPING_TERM 100になっていて原因調査にとても困った経験があるため、
+// あまり美しくないが一律デフォルト値(200)へリセットしている
 #undef TAPPING_TERM
 
 #define TAPPING_TERM 200
 
-#define COMBO_COUNT  3
+#define COMBO_COUNT 3
