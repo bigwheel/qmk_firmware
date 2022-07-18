1f44040cb6f8befe4524b769bc02b2f2c2ea901b
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-18 16:20:14 +0900

    Revert "IME無効化キーをdefault layerではなくrules.mkの変数依存に変更"
    This reverts commit 6899c24263d0631157c6fa762d9b2b08fe58a76c.

--- users/com4table/rules.mk
+++ users/com4table/rules.mk
@@ -1,11 +1,6 @@
 SRC += \
 	   util_km.c \
 	   features.c \
+	   disable_ime.c \
 	   auto_disable_ime_timer.c \
 	   auto_disable_ime.c
-
-ifeq ($(MAC_KEYMAP),1)
-SRC += disable_ime_mac.c
-else
-SRC += disable_ime_pc.c
-endif
