338e55b15fc68dc082d90fa7c9d7096273ebc212
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-18 14:13:20 +0900

    マクロ定義を整理

--- keyboards/kagizaraya/halberd/keymaps/com4table/config_macro.c
+++ keyboards/kagizaraya/halberd/keymaps/com4table/config_macro.c
@@ -1,11 +1,12 @@
 #include QMK_KEYBOARD_H
+#include "virtual_keycode_pc.h"
 
-const uint16_t PROGMEM combo_inputs0[] = { KC_TAB, KC_Q, COMBO_END };
-const uint16_t PROGMEM combo_inputs1[] = { KC_COMMA, KC_DOT, KC_SLSH, COMBO_END };
-const uint16_t PROGMEM combo_inputs2[] = { KC_L, KC_SCLN, COMBO_END };
+const uint16_t PROGMEM combo_inputs0[] = {KC_TAB, KC_Q, COMBO_END};
+const uint16_t PROGMEM combo_inputs1[] = {KC_COMMA, KC_DOT, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM combo_inputs2[] = {KC_Z, KC_X, KC_C, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_inputs0, KC_ESC),
-    COMBO(combo_inputs1, KC_LGUI),
-    COMBO(combo_inputs2, KC_LALT),
+    COMBO(combo_inputs1, RIGHT_OF_LEFT_CTRL_KEY),
+    COMBO(combo_inputs2, RIGHT_OF_LEFT_CTRL_KEY),
 };
