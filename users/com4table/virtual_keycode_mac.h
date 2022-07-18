801f222c8cbffa09d103c1a057a551920fc23b3c
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-18 16:17:47 +0900

    プリプロセッサを活用する方向に舵を切る

--- users/com4table/virtual_keycode_mac.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include QMK_KEYBOARD_H
-
-#define LEFT_OF_LEFT_LANG_KEY KC_LGUI
-#define RIGHT_OF_LEFT_CTRL_KEY KC_LALT
-#define LEFT_LANG_KEY KC_LNG2
-#define RIGHT_LANG_KEY KC_LNG1
