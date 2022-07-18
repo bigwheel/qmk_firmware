b458b1f31327b25ffe99314709f0f9c898a27275
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-18 13:00:30 +0900

    OS間のキー差分を吸収する機能を作ったが、これが正解か確信が持てない

--- users/con4t4blev2/config.h
+++ users/con4t4blev2/config.h
@@ -5,9 +5,3 @@
 #undef TAPPING_TERM
 
 #define TAPPING_TERM 200
-
-// 最初if definedでやろうとしたが、このconfig.hがkeyboards/xxx/keymaps下の
-// config.hより先に読まれるようでPC_KEYMAPが定義される前にここが評価されてしまうようだ
-// https://github.com/bigwheel/qmk_firmware/blob/b703d4a3bd8f752ae012caefddf699b4e03552e6/users/con4t4blev2/config.h
-// なので、マクロ関数にして評価タイミングを keymap下のコードが処理されるところまで遅延させた
-#define LEFT_OF_LEFT_LANG_KEY() (MAC_KEYMAP ? KC_LALT : KC_LGUI)
---
+++ users/con4t4blev2/virtual_keycode_mac.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+#define LEFT_OF_LEFT_LANG_KEY KC_LGUI
