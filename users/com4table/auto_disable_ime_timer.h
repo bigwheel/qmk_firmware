d70675bfa3a4f2bca566ec0666e9b36665d840f3
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-23 06:10:39 +0900

    submodule化のために一度削除

--- users/com4table/auto_disable_ime_timer.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void matrix_scan_user_auto_disable_ime_timer(void);
-
-void process_record_user_auto_disable_ime_timer(void);
--- users/com4table/rules.mk
+++
@@ -1,7 +0,0 @@
-SRC += \
-	   util_km.c \
-	   features.c \
-	   disable_ime.c \
-	   auto_disable_ime_timer.c \
-     qmk_backward_func.c \
-	   auto_disable_ime.c
